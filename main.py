6ff08502dc18d6ed8aa293fa876f51a6402c261b
khockenj <slyster1010@gmail.com>
2019-10-03 17:11:15 -0400

    updates

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ app = Flask(__name__,
             static_folder = "./dist/static",
             template_folder = "./dist")
 #CORS(app)
+#only need cors when local
 #app.config["MONGO_URI"] = "mongodb://localhost:27017/lom"  
 app.config["MONGO_URI"] = "mongodb+srv://admin2:etnl4OefU7uuTh00@lom-wlgkz.gcp.mongodb.net/lom?retryWrites=true&w=majority"
 
@@ -20,6 +21,7 @@ mongo = PyMongo(app)
 mongo.db.users.create_index([('name', 'text')])   
 @app.route('/')
 def index():
+    generateCode()
     if request.cookies.get('user') and request.cookies.get('duser'):
         loggedIn = True
     elif request.cookies.get('rememberMe') and request.cookies.get('duser'):
@@ -40,13 +42,16 @@ def login():
     r = request.args.get('r')
     mcode = request.args.get('mcode')
     if not request.cookies.get('user') and not request.cookies.get('rememberMe') and r != "1":
-        return redirect("https://discordapp.com/api/oauth2/authorize?client_id=628252746365140999&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fcallback%3Fr%3D0&response_type=code&scope=identify%20email%20guilds.join")
+        return redirect('https://discordapp.com/api/oauth2/authorize?client_id=628252746365140999&redirect_uri=https%3A%2F%2Flom-website-253818.appspot.com%2Fcallback%3Fr%3D0%26m%3D0&response_type=code&scope=identify%20email%20guilds.join')
+        #return redirect("https://discordapp.com/api/oauth2/authorize?client_id=628252746365140999&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fcallback%3Fr%3D0&response_type=code&scope=identify%20email%20guilds.join")
     elif not request.cookies.get('user') and not request.cookies.get('rememberMe') and r == "1":
         if mcode:
             mongo.db.codes.update({'code': mcode},{'$set':{"used" : True, "usedBy": ''}})
-            return redirect('https://discordapp.com/api/oauth2/authorize?client_id=628252746365140999&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fcallback%3Fr%3D1%26m%3D1&response_type=code&scope=identify%20email%20guilds.join')
+            return redirect("https://discordapp.com/api/oauth2/authorize?client_id=628252746365140999&redirect_uri=https%3A%2F%2Flom-website-253818.appspot.com%2Fcallback%3Fr%3D1%26m%3D1&response_type=code&scope=identify%20guilds.join%20email")
+            #return redirect('https://discordapp.com/api/oauth2/authorize?client_id=628252746365140999&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fcallback%3Fr%3D1%26m%3D1&response_type=code&scope=identify%20email%20guilds.join')
         else:
-            return redirect('https://discordapp.com/api/oauth2/authorize?client_id=628252746365140999&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fcallback%3Fr%3D1%26m%3D0&response_type=code&scope=identify%20email%20guilds.join')
+            return redirect('https://discordapp.com/api/oauth2/authorize?client_id=628252746365140999&redirect_uri=https%3A%2F%2Flom-website-253818.appspot.com%2Fcallback%3Fr%3D1%26m%3D0&response_type=code&scope=identify%20email%20guilds.join')
+            #return redirect('https://discordapp.com/api/oauth2/authorize?client_id=628252746365140999&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fcallback%3Fr%3D1%26m%3D0&response_type=code&scope=identify%20email%20guilds.join')
     else:
         user = mongo.db.users.find_one({"did": request.cookies.get("duser")})
         response = make_response(redirect('/#/profile/' + user['name']))
@@ -76,7 +81,8 @@ def callback():
     params = {
         "client_id" : CLIENT_ID,
         "client_secret" : CLIENT_SECRET,
-        "redirect_uri" : "http://localhost:5000/callback?r=" + r + "&m=" + m,
+        #"redirect_uri" : "http://localhost:5000/?r=" + r + "&m=" + m,
+        "redirect_uri" : "https://lom-website-253818.appspot.com/?r=" + r + "&m=" + m,
         "grant_type":"authorization_code",
         "code" : code,
         "scope": "identify email guilds.join"
